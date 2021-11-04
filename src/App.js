bc0db9debf1233819367d31e3c2da64bcd1f3b75
David-Ibikunle <sinmi2404@gmail.com>
2021-11-04 22:58:27 +0100

    started work on sign in page. created, sign-in, form-input and button components

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/hompage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up-page';
+import Header from './components/header/header.component';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component ={ShopPage} />
+        <Route path='/signin' component ={SignInAndSignUpPage} />
       </Switch>
       
     </div>
