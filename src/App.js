d92f3ca3840466585c51b1e62a79a4b6a174398b
David-Ibikunle <sinmi2404@gmail.com>
2021-11-04 01:36:28 +0100

    Created Header component and some other improvements

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/hompage.component';
-import ShopPage from './pages/shop/shop.component.jsx'
-
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component ={ShopPage} />
